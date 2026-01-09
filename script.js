cfcd07e1b25828a8d7e39e081fc4cf647b4a3378
Jay bhardwaj <Jaygopalbhardwaj1617@gmail.com>
2026-01-09 15:31:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,10 @@
-const table = document.querySelector("table");
-const tBody = table.lastElementChild;
-
-const newRow = document.createElement("tr");
-tBody.append(newRow);
-
-const newData1 = document.createElement("td");
-const button = document.createElement("button");
-button.innerText = "Total";
-newData1.appendChild(button);
-
-const newData2 = document.createElement("td");
-newData2.setAttribute('id', 'ans');
-
-newRow.append(newData1);
-newRow.append(newData2);
-
 button.addEventListener("click", () => {
   const prices = document.querySelectorAll(".price");
   let sum = 0;
+  for (let price of prices) {
+    // Convert contentEditable text to number safely
+    sum += Number(price.innerText) || 0;
+  }
   for (let price of prices) sum += Number(price.innerText) || 0;
   newData2.innerText = sum;
 });
